680107b4e7178d923507a44008875d7008e90d56
lveay2 <wangzsfs@gmail.com>
2021-01-23 12:59:18 -0500

    add more

---
+++ src/main/java/lintcode/dfs/_291_Second_Diameter.java
@@ -0,0 +1,15 @@
+package lintcode.dfs;
+
+public class _291_Second_Diameter {
+
+    public long getSecondDiameter(int[][] edge) {
+
+        return 0L;
+    }
+
+    public static void main(String[] args) {
+        _291_Second_Diameter solution = new _291_Second_Diameter();
+        System.out.println(solution.getSecondDiameter(new int[][]{{0, 1, 4}, {0, 2, 7}}));
+    }
+
+}
---
+++ src/main/java/lintcode/dynamic_programming/_111_Climbing_Stairs.java
@@ -0,0 +1,29 @@
+package lintcode.dynamic_programming;
+
+public class _111_Climbing_Stairs {
+
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return n = 0;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        _111_Climbing_Stairs solution = new _111_Climbing_Stairs();
+        System.out.println(solution.climbStairs(0));
+        System.out.println(solution.climbStairs(1));
+        System.out.println(solution.climbStairs(2));
+        System.out.println(solution.climbStairs(10));
+    }
+
+}
