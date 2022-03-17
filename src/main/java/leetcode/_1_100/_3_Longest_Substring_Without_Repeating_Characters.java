4c6cc6ab64c12d7f074661a3bd4fa419f8c40789
lveay2 <wangzsfs@gmail.com>
2022-03-17 15:51:47 -0400

    add optimized solution of lengthOfLongestSubstring

--- src/main/java/leetcode/_1_100/_3_Longest_Substring_Without_Repeating_Characters.java
+++ src/main/java/leetcode/_1_100/_3_Longest_Substring_Without_Repeating_Characters.java
@@ -33,7 +33,38 @@ public class _3_Longest_Substring_Without_Repeating_Characters {
     public static void main(String[] args) {
         System.out.println("1 == " + lengthOfLongestSubstring(" "));
         System.out.println("2 == " + lengthOfLongestSubstring("abba"));
+        System.out.println("2 == " + lengthOfLongestSubstring("abbbbbbbbbbbba"));
         System.out.println("3 == " + lengthOfLongestSubstring("abcabcbb"));
+        System.out.println("3 == " + lengthOfLongestSubstring("pwwkew"));
+
+        System.out.println("2 == " + lengthOfLongestSubstring("abba"));
+        System.out.println("2 == " + lengthOfLongestSubstring("abbbbbbbbbbbba"));
+        System.out.println("3 == " + lengthOfLongestSubstring2("abcabcbb"));
+        System.out.println("3 == " + lengthOfLongestSubstring2("pwwkew"));
+    }
+
+    public static int lengthOfLongestSubstring2(String s) {
+
+        Map<Character, Integer> map = new HashMap<>();
+        int start = 0;
+        int max = Integer.MIN_VALUE;
+        for (int end = 0; end < s.length(); end++) {
+            char c = s.charAt(end);
+
+//            System.out.println("start: " + start + ", end: " + end + " char: " + c + " index: " + map.get(c));
+//            System.out.println("map:" + map);
+            if (map.containsKey(c)) {
+                start = Math.max(start, map.get(c));
+            }
+//            System.out.println("start: " + start + ", end: " + end + " char: " + c + " index: " + map.get(c));
+//            System.out.println("map:" + map);
+
+            max = Math.max(max, end - start + 1);
+            map.put(c, end + 1);
+//            System.out.println("max: " + max + " map:" + map);
+        }
+
+        return max;
     }
 
 }
