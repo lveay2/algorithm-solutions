450a4d69a3e9186942cf3e7f50013b5b7bf2bd78
lveay2 <7659293+lveay2@users.noreply.github.com>
2022-04-03 21:08:03 -0400

    update _51_NQueens

--- src/main/java/leetcode/_1_100/_51_NQueens.java
+++ src/main/java/leetcode/_1_100/_51_NQueens.java
@@ -56,17 +56,21 @@ public class _51_NQueens {
     private static boolean isValid(char[][] board, int row, int col) {
         int n = board.length;
 
-        for (int i = 0; i < n; i++) {
+        // up side no Q
+        for (int i = row; i >= 0; i--) {
             if (board[i][col] == 'Q') {
                 return false;
             }
         }
 
+        // left side no Q
         for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
             if (board[i][j] == 'Q') {
                 return false;
             }
         }
+
+        // right side no Q
         for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
             if (board[i][j] == 'Q') {
                 return false;
