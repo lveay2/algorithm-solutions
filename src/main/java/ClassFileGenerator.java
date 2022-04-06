e596e466844396d6330f18aef5a13d80e5befe0a
lveay2 <7659293+lveay2@users.noreply.github.com>
2022-04-06 00:55:33 -0400

    update _304_RangeSumQueryImmutable

--- src/main/java/ClassFileGenerator.java
+++ src/main/java/ClassFileGenerator.java
@@ -10,7 +10,7 @@ public class ClassFileGenerator {
   private static final String PACKAGE_OA = "oa";
 
   public static void main(String[] args) throws IOException {
-    String problemName = "303. Range Sum Query - Immutable";
+    String problemName = "304. Range Sum Query 2D - Immutable\n";
     createLeetCodeProblemFile(problemName);
 
     problemName = "1495 · Leaf-Similar Trees";
---
+++ src/main/java/leetcode/_301_400/_304_RangeSumQueryImmutable.java
@@ -0,0 +1,72 @@
+package leetcode._301_400;
+
+/*
+304. Range Sum Query 2D - Immutable
+Given a 2D matrix matrix, handle multiple queries of the following type:
+
+Calculate the sum of the elements of matrix inside the rectangle defined
+by its upper left corner (row1, col1) and lower right corner (row2, col2).
+Implement the NumMatrix class:
+
+NumMatrix(int[][] matrix) Initializes the object with the integer matrix matrix.
+int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of
+the elements of matrix inside the rectangle defined by its upper left corner
+(row1, col1) and lower right corner (row2, col2).
+
+Example 1:
+Input
+["NumMatrix", "sumRegion", "sumRegion", "sumRegion"]
+[[[[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]], [2, 1, 4, 3], [1, 1, 2, 2], [1, 2, 2, 4]]
+Output
+[null, 8, 11, 12]
+
+Explanation
+NumMatrix numMatrix = new NumMatrix([[3, 0, 1, 4, 2], [5, 6, 3, 2, 1], [1, 2, 0, 1, 5], [4, 1, 0, 1, 7], [1, 0, 3, 0, 5]]);
+numMatrix.sumRegion(2, 1, 4, 3); // return 8 (i.e sum of the red rectangle)
+numMatrix.sumRegion(1, 1, 2, 2); // return 11 (i.e sum of the green rectangle)
+numMatrix.sumRegion(1, 2, 2, 4); // return 12 (i.e sum of the blue rectangle)
+
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 200
+-105 <= matrix[i][j] <= 105
+0 <= row1 <= row2 < m
+0 <= col1 <= col2 < n
+At most 104 calls will be made to sumRegion.
+ */
+public class _304_RangeSumQueryImmutable {
+
+  public static void main(String[] args) {
+    NumMatrix numMatrix =
+        new NumMatrix(
+            new int[][] {
+              {3, 0, 1, 4, 2}, {5, 6, 3, 2, 1}, {1, 2, 0, 1, 5}, {4, 1, 0, 1, 7}, {1, 0, 3, 0, 5}
+            });
+    System.out.println("8 == " + numMatrix.sumRegion(2, 1, 4, 3));
+    System.out.println("11 == " + numMatrix.sumRegion(1, 1, 2, 2));
+    System.out.println("12 == " + numMatrix.sumRegion(1, 2, 2, 4));
+  }
+
+  static class NumMatrix {
+
+    int[][] sum = null;
+
+    public NumMatrix(int[][] matrix) {
+      int m = matrix.length;
+      int n = matrix[0].length;
+      sum = new int[m + 1][n + 1];
+      for (int i = 1; i < m + 1; i++) {
+        for (int j = 1; j < n + 1; j++) {
+          // matrix[0][0], [i][j] = matrix[i][j] + sum([0][0], [i -1][j])
+          // + sum([0][0], [i][j-1]) - sum([0][0], [i-1][j-1])
+          sum[i][j] = matrix[i - 1][j - 1] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
+        }
+      }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+      return sum[row2 + 1][col2 + 1] - sum[row2 + 1][col1] - sum[row1][col2 + 1] + sum[row1][col1];
+    }
+  }
+}
