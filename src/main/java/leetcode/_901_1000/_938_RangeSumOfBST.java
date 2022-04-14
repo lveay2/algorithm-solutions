4035baefa9d686b82b99ce476cdac196f4f1fea9
lveay2 <7659293+lveay2@users.noreply.github.com>
2022-04-14 00:10:01 -0400

    add _938_RangeSumOfBST

---
+++ src/main/java/leetcode/_901_1000/_938_RangeSumOfBST.java
@@ -0,0 +1,55 @@
+package leetcode._901_1000;
+
+import common.TreeNode;
+
+/*
+938. Range Sum of BST
+Given the root node of a binary search tree and two integers
+low and high, return the sum of values of all nodes with a
+value in the inclusive range [low, high].
+
+Example 1:
+Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+Output: 32
+Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
+
+Example 2:
+Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+Output: 23
+Explanation: Nodes 6, 7, and 10 are in the range [6, 10]. 6 + 7 + 10 = 23.
+
+Constraints:
+The number of nodes in the tree is in the range [1, 2 * 104].
+1 <= Node.val <= 105
+1 <= low <= high <= 105
+All Node.val are unique.
+ */
+public class _938_RangeSumOfBST {
+
+  public static void main(String[] args) {
+    System.out.println("32 == " + rangeSumBST(TreeNode.buildTree("10,5,15,3,7,null,18"), 7, 15));
+    System.out.println(
+        "23 == " + rangeSumBST(TreeNode.buildTree("10,5,15,3,7,13,18,1,null,6"), 6, 10));
+  }
+
+  static int sum = 0;
+
+  public static int rangeSumBST(TreeNode root, int low, int high) {
+    sum = 0;
+    dfs(root, low, high);
+    return sum;
+  }
+
+  public static void dfs(TreeNode root, int low, int high) {
+    if (root == null) {
+      return;
+    }
+
+    if (root.val >= low && root.val <= high) {
+      sum += root.val;
+    }
+
+    dfs(root.left, low, high);
+    dfs(root.right, low, high);
+  }
+}
