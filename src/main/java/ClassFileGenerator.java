7543e62b15590c5cdc839bf38fdd52933cecc1e9
lveay2 <7659293+lveay2@users.noreply.github.com>
2022-04-14 00:51:11 -0400

    add update _314_BinaryTreeVerticalOrderTraversal

--- src/main/java/ClassFileGenerator.java
+++ src/main/java/ClassFileGenerator.java
@@ -10,7 +10,7 @@ public class ClassFileGenerator {
   private static final String PACKAGE_OA = "oa";
 
   public static void main(String[] args) throws IOException {
-    String problemName = "938. Range Sum of BST";
+    String problemName = "314. Binary Tree Vertical Order Traversal\n";
     createLeetCodeProblemFile(problemName);
 
     problemName = "1495 · Leaf-Similar Trees";
---
+++ src/main/java/leetcode/_301_400/_314_BinaryTreeVerticalOrderTraversal.java
@@ -0,0 +1,84 @@
+package leetcode._301_400;
+
+import common.TreeNode;
+
+import java.util.*;
+
+/*
+314. Binary Tree Vertical Order Traversal
+Given the root of a binary tree, return the vertical order traversal of
+its nodes' values. (i.e., from top to bottom, column by column).
+
+If two nodes are in the same row and column, the order should be from
+left to right.
+
+Example 1:
+Input: root = [3,9,20,null,null,15,7]
+Output: [[9],[3,15],[20],[7]]
+
+Example 2:
+Input: root = [3,9,8,4,0,1,7]
+Output: [[4],[9],[3,0,1],[8],[7]]
+
+Example 3:
+Input: root = [3,9,8,4,0,1,7,null,null,null,2,5]
+Output: [[4],[9,5],[3,0,1],[8,2],[7]]
+
+Constraints:
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ */
+public class _314_BinaryTreeVerticalOrderTraversal {
+
+  public static void main(String[] args) {
+    System.out.println("[[2], [1], [3]] ==\n" + verticalOrder(TreeNode.buildTree("1,2,3")));
+    System.out.println(
+        "[[9], [3, 15], [20], [7]] ==\n" + verticalOrder(TreeNode.buildTree("3,9,20,null,null,15,7")));
+    System.out.println(
+        "[[4], [9], [3, 0, 1], [8], [7]] ==\n" + verticalOrder(TreeNode.buildTree("3,9,8,4,0,1,7")));
+    System.out.println(
+        "[[4], [9, 5], [3, 0, 1], [8, 2], [7]] ==\n"
+            + verticalOrder(TreeNode.buildTree("3,9,8,4,0,1,7,null,null,null,2,5")));
+  }
+
+  public static List<List<Integer>> verticalOrder(TreeNode root) {
+    if (root == null) {
+      return Collections.emptyList();
+    }
+    List<List<Integer>> result = new ArrayList<>();
+
+    Queue<TreeNode> queue = new LinkedList<>();
+    queue.offer(root);
+
+    Map<TreeNode, Integer> n2c = new HashMap<>();
+    n2c.put(root, 0);
+
+    Map<Integer, List<Integer>> col2List = new TreeMap<>();
+
+    int min = Integer.MAX_VALUE;
+    while (!queue.isEmpty()) {
+      TreeNode node = queue.poll();
+      int col = n2c.get(node);
+      min = Math.min(min, col);
+
+      col2List.putIfAbsent(col, new ArrayList<>());
+      col2List.get(col).add(node.val);
+
+      if (node.left != null) {
+        queue.offer(node.left);
+        n2c.put(node.left, col - 1);
+      }
+
+      if (node.right != null) {
+        queue.offer(node.right);
+        n2c.put(node.right, col + 1);
+      }
+    }
+
+    while (col2List.containsKey(min)) {
+      result.add(col2List.get(min++));
+    }
+
+    return result;
+  }
+}
