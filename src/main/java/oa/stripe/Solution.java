e1d1198aecfbe554d660d414d3052d6424da60c7
Zhe Wang <zhe@hioscar.com>
2022-05-03 12:24:03 -0400

    update

--- src/main/java/oa/stripe/Solution.java
+++ src/main/java/oa/stripe/Solution.java
@@ -7,12 +7,17 @@ package oa.stripe;
  * If you need more classes, simply define them inline.
  */
 
-
 // Your previous Plain Text content is preserved below:
 
-// At Stripe, we send a lot of invoices. For each invoice, we send multiple reminder emails. For this question, you will be outputting the subject line of each email we send for customers' invoices in sorted order. (Use any library/tool for sorting).
+// At Stripe, we send a lot of invoices. For each invoice, we send multiple reminder emails. For
+// this question, you will be outputting the subject line of each email we send for customers'
+// invoices in sorted order. (Use any library/tool for sorting).
 
-// Your invoicing system will need to be able to configure a reminder schedule. For example, you might want to send one email 10 days before the invoice comes out, one email when the invoice comes out, one email 10 days before the invoice is due, and one email when the invoice is due, which is 30 days from when the invoice first came out. This send schedule could look something like this, corresponding to the diagram below:
+// Your invoicing system will need to be able to configure a reminder schedule. For example, you
+// might want to send one email 10 days before the invoice comes out, one email when the invoice
+// comes out, one email 10 days before the invoice is due, and one email when the invoice is due,
+// which is 30 days from when the invoice first came out. This send schedule could look something
+// like this, corresponding to the diagram below:
 
 // ```
 //      │
@@ -40,7 +45,11 @@ package oa.stripe;
 //      ▼
 // ```
 
-// Given a configuration for sending emails, your input will be an unsorted list of unique customer invoices with times for when their `[New]` invoice should come out and how much we are charging them in dollars. Your objective is to print out the subject lines of all emails we will send out in sorted order by time. Your output should include the time, the type of email, the customer, and amount due.
+// Given a configuration for sending emails, your input will be an unsorted list of unique customer
+// invoices with times for when their `[New]` invoice should come out and how much we are charging
+// them in dollars. Your objective is to print out the subject lines of all emails we will send out
+// in sorted order by time. Your output should include the time, the type of email, the customer,
+// and amount due.
 
 // Here is a sample of how your system should behave:
 
@@ -86,59 +95,62 @@ package oa.stripe;
 import java.util.*;
 
 class Solution {
-    public static void main(String[] args) {
-        Map<Integer, String> sendSchedule = new HashMap<>();
-        sendSchedule.put(-10, "Upcoming");
-        sendSchedule.put(0, "New");
-        sendSchedule.put(20, "Reminder");
-        sendSchedule.put(30, "Due");
+  public static void main(String[] args) {
+    Map<Integer, String> sendSchedule = new HashMap<>();
+    sendSchedule.put(-10, "Upcoming");
+    sendSchedule.put(0, "New");
+    sendSchedule.put(20, "Reminder");
+    sendSchedule.put(30, "Due");
 
-        Invoicer invoicer = Invoicer.newInstance(sendSchedule);
+    Invoicer invoicer = Invoicer.newInstance(sendSchedule);
 
-        // customer_invoices = [
-//     {"invoice_time": 0, "name": "Alice", "amount": 200},
-//     {"invoice_time": 1, "name": "Bob", "amount": 100},
-// ]
-        Map<String, String> customer_invoice = new HashMap<>();
-        customer_invoice.put("invoice_time", "0");
-        customer_invoice.put("name", "Alice");
-        customer_invoice.put("amount", "200");
+    // customer_invoices = [
+    //     {"invoice_time": 0, "name": "Alice", "amount": 200},
+    //     {"invoice_time": 1, "name": "Bob", "amount": 100},
+    // ]
+    Map<String, String> customer_invoice = new HashMap<>();
+    customer_invoice.put("invoice_time", "0");
+    customer_invoice.put("name", "Alice");
+    customer_invoice.put("amount", "200");
 
-        Map<String, String> customer_invoice2 = new HashMap<>();
-        customer_invoice2.put("invoice_time", "1");
-        customer_invoice2.put("name", "Bob");
-        customer_invoice2.put("amount", "100");
+    Map<String, String> customer_invoice2 = new HashMap<>();
+    customer_invoice2.put("invoice_time", "1");
+    customer_invoice2.put("name", "Bob");
+    customer_invoice2.put("amount", "100");
 
-        List<Map<String, String>> customer_invoices = new ArrayList<>();
-        customer_invoices.add(customer_invoice);
-        customer_invoices.add(customer_invoice2);
+    List<Map<String, String>> customer_invoices = new ArrayList<>();
+    customer_invoices.add(customer_invoice);
+    customer_invoices.add(customer_invoice2);
 
+    System.out.println("\nSolution 1");
+    invoicer.sendEmails(customer_invoices);
 
-// customer_payments = [
-//     {:payment_time=>-9, :name=>"Alice", :amount=>100},
-//     {:payment_time=>1, :name=>"Alice", :amount=>50},
-//     {:payment_time=>0, :name=>"Bob", :amount=>100},
-// ]
+    // customer_payments = [
+    //     {:payment_time=>-9, :name=>"Alice", :amount=>100},
+    //     {:payment_time=>1, :name=>"Alice", :amount=>50},
+    //     {:payment_time=>0, :name=>"Bob", :amount=>100},
+    // ]
 
+    System.out.println("\nSolution 2");
 
-        Map<String, String> customer_payment = new HashMap<>();
-        customer_payment.put("payment_time", "-9");
-        customer_payment.put("name", "Alice");
-        customer_payment.put("amount", "100");
-        Map<String, String> customer_payment1 = new HashMap<>();
-        customer_payment1.put("payment_time", "1");
-        customer_payment1.put("name", "Alice");
-        customer_payment1.put("amount", "50");
-        Map<String, String> customer_payment2 = new HashMap<>();
-        customer_payment2.put("payment_time", "0");
-        customer_payment2.put("name", "Bob");
-        customer_payment2.put("amount", "100");
+    Map<String, String> customer_payment = new HashMap<>();
+    customer_payment.put("payment_time", "-9");
+    customer_payment.put("name", "Alice");
+    customer_payment.put("amount", "100");
+    Map<String, String> customer_payment1 = new HashMap<>();
+    customer_payment1.put("payment_time", "1");
+    customer_payment1.put("name", "Alice");
+    customer_payment1.put("amount", "50");
+    Map<String, String> customer_payment2 = new HashMap<>();
+    customer_payment2.put("payment_time", "0");
+    customer_payment2.put("name", "Bob");
+    customer_payment2.put("amount", "100");
 
-        List<Map<String, String>> customer_payments = new ArrayList<>();
-        customer_payments.add(customer_payment);
-        customer_payments.add(customer_payment1);
-        customer_payments.add(customer_payment2);
+    List<Map<String, String>> customer_payments = new ArrayList<>();
+    customer_payments.add(customer_payment);
+    customer_payments.add(customer_payment1);
+    customer_payments.add(customer_payment2);
 
-        invoicer.sendEmails(customer_invoices, customer_payments);
-    }
+    invoicer.sendEmails(customer_invoices, customer_payments);
+  }
 }
