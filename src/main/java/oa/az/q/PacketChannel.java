15e5dab70d7834c29d3470f2168184a59d768d8b
lveay2 <7659293+lveay2@users.noreply.github.com>
2022-07-10 22:28:28 -0400

    packets solution

---
+++ src/main/java/oa/az/q/PacketChannel.java
@@ -0,0 +1,46 @@
+package oa.az.q;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class PacketChannel {
+
+  public static void main(String[] args) {
+    System.out.println(maximumQuality(Arrays.asList(1,2,3,4,5), 2));
+    System.out.println("----");
+    System.out.println(maximumQuality(Arrays.asList(5, 2, 2, 1, 5, 3), 2));
+  }
+
+  private static long maximumQuality(List<Integer> packets, int channels) {
+    int pSize = packets.size();
+    int ans = 0;
+    if (pSize == channels) {
+      for (int p: packets) {
+        ans += p;
+      }
+      return (long) ans;
+    }
+
+    Collections.sort(packets);
+    for (int i = 1; i <= channels - 1; i++) {
+      ans += packets.get(pSize - i);
+    }
+    System.out.println(ans);
+
+    int remain = pSize - channels + 1;
+    if (remain % 2 == 0) {
+      double doubleValue = Math.ceil((double)(packets.get((remain-1)/2) + packets.get(((remain-1)/2) + 1)) / 2);
+      System.out.println(doubleValue);
+      ans += doubleValue;
+      System.out.println(ans + " " + packets.get((remain-1)/2)+ " " + packets.get(((remain-1)/2) + 1));
+    } else {
+      ans += packets.get(remain/2);
+      System.out.println(ans + " " + packets.get((remain-1)/2));
+    }
+
+    return ans;
+  }
+
+
+}
