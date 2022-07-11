8fe4d7d09d173c1084ac31d95a49403715474457
lveay2 <7659293+lveay2@users.noreply.github.com>
2022-07-10 22:37:41 -0400

    format

--- src/main/java/oa/az/q/PacketChannel.java
+++ src/main/java/oa/az/q/PacketChannel.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class PacketChannel {
 
   public static void main(String[] args) {
-    System.out.println(maximumQuality(Arrays.asList(1,2,3,4,5), 2));
+    System.out.println(maximumQuality(Arrays.asList(1, 2, 3, 4, 5), 2));
     System.out.println("----");
     System.out.println(maximumQuality(Arrays.asList(5, 2, 2, 1, 5, 3), 2));
   }
@@ -16,7 +16,7 @@ public class PacketChannel {
     int pSize = packets.size();
     long ans = 0;
     if (pSize == channels) {
-      for (int p: packets) {
+      for (int p : packets) {
         ans += p;
       }
       return ans;
@@ -26,21 +26,25 @@ public class PacketChannel {
     for (int i = 1; i <= channels - 1; i++) {
       ans += packets.get(pSize - i);
     }
-    System.out.println(ans);
 
     int remain = pSize - channels + 1;
     if (remain % 2 == 0) {
-      double doubleValue = Math.ceil((double)(packets.get((remain-1)/2) + packets.get(((remain-1)/2) + 1)) / 2);
+      System.out.println(
+          ans + " " + packets.get((remain - 1) / 2) + " " + packets.get(((remain - 1) / 2) + 1));
+
+      double doubleValue =
+          Math.ceil(
+              (double) (packets.get((remain - 1) / 2) + packets.get(((remain - 1) / 2) + 1)) / 2);
       System.out.println(doubleValue);
+
       ans += doubleValue;
-      System.out.println(ans + " " + packets.get((remain-1)/2)+ " " + packets.get(((remain-1)/2) + 1));
+
     } else {
-      ans += packets.get(remain/2);
-      System.out.println(ans + " " + packets.get((remain-1)/2));
+      System.out.println(ans + " " + packets.get((remain - 1) / 2));
+
+      ans += packets.get(remain / 2);
     }
 
     return ans;
   }
-
-
 }
